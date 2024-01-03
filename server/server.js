c2385ded90075fd4904e291db26aea36d168bba4
Smile <singhswapnil060303@gmail.com>
2024-01-03 18:39:01 +0530

    removed the unwanted line in the files

--- server/server.js
+++ server/server.js
@@ -10,7 +10,6 @@ const elasticsearch = require("elasticsearch");
 
 const { Client } = require('@elastic/elasticsearch');
 
-//const esClient = new Client({ node: 'http://localhost:9200' });
 
 const PORT = process.env.PORT || 4000;
 const database = require("./config/database");
@@ -29,24 +28,12 @@ app.listen(PORT, ()=> {
 
 
 const { MongoClient } = require('mongodb');
-//const { Client } = require('@elastic/elasticsearch');
 
 
 // Elasticsearch Connection
 const esClient = new Client({ node: 'http://localhost:9200' });
 
 
-
-
-
-
-  // Note: You may need to handle Elasticsearch connection closure based on your application's requirements.
-
-
-
-
-
-//const mongodbCollection = mongoClient.db('yourDatabase').collection('yourCollection');
 
 //Index data in Elasticsearch
 async function indexDataInElasticsearch(data) {
@@ -85,16 +72,6 @@ indexDataInElasticsearch()
 //     }
 //   });
 
-  app.get("/institutes", async (req, res) => {
-    try {
-      const institute = await Institute.find();
-      res.status(200).send(institute);
-    } catch (error) {
-      res.status(500).send("Error:", error);
-      console.log(error);
-    }
-  });
-
 //   async function searchInElasticsearch(query) {
 //     const { body } = await esClient.search({
 //       index: 'aicte',
@@ -113,36 +90,5 @@ indexDataInElasticsearch()
 //     console.log(result);
 //   });
 
-    app.get("/institutes/:id", async (req, res) => {
-      try {
-        const institute = await Institute.findById({_id: req.params.id});
-        res.status(200).send(institute);
-      } catch (error) {
-        res.status(400).send("Error:", error);
-        console.log(error);
-      }
-    });
-
-app.put("/updateInstitute/:id", async (req, res) => {
-    const id = req.params.id;
-    try {
-        const result = await Institute.findByIdAndUpdate({ _id: id }, req.body, {
-        new: true,
-        });
-        res.status(200).send("User has been updated.");
-        } catch (error) {
-        res.status(400).send("Error:", error);
-        console.log(error);
-        }
-    });
-
-app.delete("/deleteInstitute/:id", async (req, res) => {    
-    try {
-        await Institute.findByIdAndDelete({ _id: req.params.id });
-        res.status(200).send("User has been deleted.");
-        } catch (error) {
-        res.status(400).send("Error:", error);
-        console.log(error);
-        }
-    });
+ 
 
