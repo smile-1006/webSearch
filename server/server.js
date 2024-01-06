43abc63a901219cd99a509ede896ba2ea5a77a4f
Smile <singhswapnil060303@gmail.com>
2024-01-06 21:59:21 +0530

    apis are not working today

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const app = express();
 const dotenv = require("dotenv");
 const bodyParser = require('body-parser');
 
-const { Client } = require('@elastic/elasticsearch');
+// const { Client } = require('@elastic/elasticsearch');
 
 const elasticsearch = require("elasticsearch");
 
@@ -13,15 +13,15 @@ const Institute = require("./models/institute");
 dotenv.config();
 
 // Elasticsearch Connection
-const esClient = new Client(
-  { node: 'http://localhost:9200' }
-  );
+// const esClient = new Client(
+//   { node: 'http://localhost:9200' }
+//   );
 
 const instituteRoutes = require("./routes/institutes");
 //routes
-app.use("/api/v1/institute", instituteRoutes);
+//app.use("/api/v1/institute", instituteRoutes);
 
-
+const elastic = require("./elastic-client");
 
 const PORT = process.env.PORT || 4000;
 const database = require("./config/database");
@@ -54,35 +54,35 @@ app.listen(PORT, ()=> {
 //   });
 // }
 // indexDataInElasticsearch()
-app.use(bodyParser.json());
-app.post("/create", async (req, res) => {
-    if (!req.body) {
-      return res.status(400).send('Request body is missing');
-    }
-    const institute = new Institute(req.body);
-    try {
-    esClient.index(
-        {
-        index: "institute",
-        type: "instituteType",
-        id: req.body.id,
-        body: req.body,
-        },
-        (err, resp, status) => {
-          if (err) {
-            console.log(err);
-          } else {
-            return res.json(institute);
-          }
-        }
-      );
-      await institute.save();
-      res.status(200).send("success " + institute._id);
-    } catch (error) {
-      res.status(400).send("Error: " + error.toString());
-      console.log(error);
-    }
-  });
+// app.use(bodyParser.json());
+// app.post("/create", async (req, res) => {
+//     if (!req.body) {
+//       return res.status(400).send('Request body is missing');
+//     }
+//     const institute = new Institute(req.body);
+//     try {
+//     esClient.index(
+//         {
+//         index: "institute",
+//         type: "instituteType",
+//         id: req.body.id,
+//         body: req.body,
+//         },
+//         (err, resp, status) => {
+//           if (err) {
+//             console.log(err);
+//           } else {
+//             return res.json(institute);
+//           }
+//         }
+//       );
+//       await institute.save();
+//       res.status(200).send("success " + institute._id);
+//     } catch (error) {
+//       res.status(400).send("Error: " + error.toString());
+//       console.log(error);
+//     }
+//   });
 
 //   async function searchInElasticsearch(query) {
 //     const { body } = await esClient.search({
