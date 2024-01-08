71d28b187cb6f9e7afdf5ee1eb4735138ad30cd1
Smile <singhswapnil060303@gmail.com>
2024-01-08 21:51:15 +0530

    Remove unused code related to Elasticsearch and MongoDB

--- server/create-index.js
+++
@@ -1,8 +0,0 @@
-const elasticClient = require("./elastic-client");
-
-const createIndex = async (indexName) => {
-  await elasticClient.indices.create({ index: indexName });
-  console.log("Index created");
-};
-
-createIndex("posts");
